784ad90f4a17763118b40116443fb6a0895398f9
gek20 <V@ffancul01>
2022-02-14 17:36:18 +0100

    path fixed for app.

--- app/run.py
+++ app/run.py
@@ -8,10 +8,10 @@ from nltk.tokenize import word_tokenize
 from flask import Flask
 from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
-from sklearn.externals import joblib
+import joblib
 from sqlalchemy import create_engine
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='template')
 
 
 def tokenize(text):
@@ -31,7 +31,7 @@ engine = create_engine('sqlite:///./data/DisasterResponse.db')
 df = pd.read_sql_table('Messages', engine)
 
 # load model
-model = joblib.load("../models/your_model_name.pkl")
+model = joblib.load("./models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
